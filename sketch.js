c1fdd872ffcc20c64bae80cf394cfa80a226db32
aaronkates <65793911+aaronkates@users.noreply.github.com>
2024-10-23 17:49:35 -0400

    yoffset up 100 pixels

--- sketch.js
+++ sketch.js
@@ -236,9 +236,9 @@ function assignLetterToGrid(i, j, letter) {
 
 function createKeyboardButtons() {
   let buttonLayout = [
-    { rowLength: 10, xOffset: -160, yOffset: 600 },
-    { rowLength: 9, xOffset: -140, yOffset: 650 },
-    { rowLength: 7, xOffset: -110, yOffset: 700 },
+    { rowLength: 10, xOffset: -160, yOffset: 500 },
+    { rowLength: 9, xOffset: -140, yOffset: 550 },
+    { rowLength: 7, xOffset: -110, yOffset: 600 },
   ];
 
   let letters = "qwertyuiopasdfghjklzxcvbnm";
